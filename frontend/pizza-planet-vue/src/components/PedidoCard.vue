8ced54c796d14037c5c621abd9f339f03ebca016
OscarM04 <oscarmarquez2218@gmail.com>
2020-08-04 21:59:36 -0400

    [Add] Anadido tarjeta pedido

---
+++ frontend/pizza-planet-vue/src/components/PedidoCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card>
+        <v-card-title>
+          <h2>Pedido</h2>
+        </v-card-title>
+        <v-card-text>
+          <h3>User: {{user}}</h3>
+          <h3>Fecha: {{date}}</h3>
+          <p> Pizza: {{pizza}}</p>
+          <p> Ingredientes: {{ingredientes}}</p>
+        </v-card-text>
+      </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PedidoCard',
+  props: {
+    user: String,
+    date: String,
+    pizza: String,
+    ingredientes: String,
+  },
+};
+</script>
--- frontend/pizza-planet-vue/src/views/Home.vue
+++ frontend/pizza-planet-vue/src/views/Home.vue
@@ -1,26 +1,44 @@
 <template>
 <v-container>
-  <v-card>
-    <v-card-title>
-      <h2>Las mejores pizzas en Pizza Planet</h2>
-    </v-card-title>
-    <v-card-text>
-      Realiza tu pedido
-    </v-card-text>
-      <br>
-      <v-btn
+  <v-row>
+    <v-col md6 v-for="pedido in listaPedidos" v-bind:key="pedido.id">
+      <PedidoCard
+        v-bind:user="pedido.user"
+        v-bind:date="pedido.date"
+        v-bind:pizza="pedido.pizza"
+        v-bind:ingredientes="pedido.ingredientes"
       >
-        +
-      </v-btn>
-  </v-card>
+      </PedidoCard>
+    </v-col>
+  </v-row>
 </v-container>
 </template>
 
 <script>
+import PedidoCard from '../components/PedidoCard.vue';
 
 export default {
   name: 'Home',
   components: {
+    PedidoCard,
   },
+  data: () => ({
+    listaPedidos: [
+      {
+        id: '1',
+        user: 'Pepito',
+        date: '04/08/2020',
+        pizza: 'Margarita',
+        ingredientes: 'Queso, Salsa de Tomate',
+      },
+      {
+        id: '2',
+        user: 'Jaimito',
+        date: '04/07/2020',
+        pizza: 'Margarita',
+        ingredientes: 'Queso, Salsa de Tomate, Tocineta',
+      },
+    ],
+  }),
 };
 </script>
