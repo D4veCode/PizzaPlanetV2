8f928150ba9bbf339fb6494ca2d17f308873a264
Your name <telodato.16@est.ucab.edu.ve>
2020-08-05 22:11:11 -0400

    Fix

--- pizzeria/pizzeriaapp/apps.py
+++ pizzeria/pizzeriaapp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PizzeriaappConfig(AppConfig):
-    name = 'pizzeriaApp'
+    name = 'pizzeriaapp'
---
+++ pizzeria/pizzeriaapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Pedido(models.Model):
+    date = models.DateField(auto_now_add=True)
+    user = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    pizza = models.CharField(max_length=50)
+    size = models.CharField(max_length=20)
+    ingredientes = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.date
