4c520f9f5001601f6e3bde395235dad7167283ae
Your name <ferdy_rod@hotmail.com>
2020-08-06 03:33:33 -0400

    Añadido Modelo Ingrediente

--- pizzeria/pizzeriaapp/models.py
+++ pizzeria/pizzeriaapp/models.py
@@ -26,3 +26,11 @@ class Base(models.Model):
 
     def __str__(self):
         return self.tamano
+
+class Ingrediente(models.Model):
+    name = models.CharField(max_length=50)
+    tamano = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name+' '+self.tamano
