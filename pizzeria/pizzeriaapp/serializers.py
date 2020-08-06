e9bb256fe28014bd5e3173f2ac343e4b1d81755c
Your name <telodato.16@est.ucab.edu.ve>
2020-08-06 10:11:49 -0400

    Fix

--- pizzeria/pizzeriaapp/serializers.py
+++ pizzeria/pizzeriaapp/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Pedido
 
-"""Esta clase se encarga de transformar el objeto a un objeto de tipo JSON""""
+"""Esta clase se encarga de transformar el objeto a un objeto de tipo JSON""
 class PedidoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Pedido
