93c43106239cf83fcbaba42854672bcca94bdd4e
David Serrada <davidasb.developer@gmail.com>
2020-08-06 00:25:05 -0400

    [fix] added router link to home

--- frontend/pizza-planet-vue/src/App.vue
+++ frontend/pizza-planet-vue/src/App.vue
@@ -14,12 +14,14 @@
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>
+              <router-link to="/" class="rlink">Home</router-link>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
+            <v-icon>mdi-pizza</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>
